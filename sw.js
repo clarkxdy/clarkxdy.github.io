8679ee68a6f9fff6d07f0c8081e9409f413de610
clarkxdy <clarkxdy@gmail.com>
2022-03-21 01:16:51 +0800

    update posts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220321.0113';
+const cacheName = 'chirpy-20220321.0116';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
