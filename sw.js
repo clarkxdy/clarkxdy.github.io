e4fab5eedc447f98df3a64b6f53be574e9a7604a
clarkxdy <clarkxdy@gmail.com>
2022-05-10 00:32:59 +0800

    0509-2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220509.2328';
+const cacheName = 'chirpy-20220510.0031';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
