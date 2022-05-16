336a023be1777e13e79aefbfc63a15e292e0b592
clarkxdy <clarkxdy@gmail.com>
2022-05-16 23:04:28 +0800

    Disable dark mode

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220515.2302';
+const cacheName = 'chirpy-20220516.2300';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
