18f923927fc12a98b5aba8c5a191b17c7b0b40f1
clarkxdy <clarkxdy@gmail.com>
2022-05-20 00:48:58 +0800

    test three.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220516.2343';
+const cacheName = 'chirpy-20220520.0048';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
